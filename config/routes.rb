432d29f7a87ae7d140325e2394dfeb780c222532
Albert <ext-albert.padzerin@sbermarket.ru>
2023-08-04 16:15:02 +0400

    Introduce posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
 end
